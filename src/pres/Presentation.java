c35a9000fbef50646c8e6c74314d72aba67647a6
steve desire <stevedesiredev@gmail.com>
2023-08-15 01:58:29 +0200

    creation du package extension avec DaoImpl2 et finition de l'instanciation statique

--- src/pres/Presentation.java
+++ src/pres/Presentation.java
@@ -1,11 +1,16 @@
 package pres;
 
 import dao.DaoImpl;
+import ext.DaoImpl2;
 import metier.MetierImpl;
 
 public class Presentation {
     public static void main(String[] args) {
-        DaoImpl dao=new DaoImpl();
+        /*
+        * Injection des dependances par instanciation statique=> new
+        * */
+       // DaoImpl dao=new DaoImpl();
+        DaoImpl2 dao=new DaoImpl2();
         MetierImpl metier=new MetierImpl();
         metier.setDao(dao);
         System.out.println("Resultat="+metier.calcul());
